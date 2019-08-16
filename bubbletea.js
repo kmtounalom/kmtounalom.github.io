60840bb666d14673166171a644d3fd126bb8b68e
kmtounalom <53197713+kmtounalom@users.noreply.github.com>
2019-08-16 10:14:30 -0700

    Add files via upload

---
+++ bubbletea.js
@@ -0,0 +1,15 @@
+//<!DOCTYPE html>
+//<html>
+//<body>
+
+//<h2>click to see the time/h2>
+
+//<button type="button"
+//onclick="document.getElementById('demo').innerHTML = Date()">
+//Click me to display Date and Time.</button>
+
+//<p id="demo"></p>
+
+//</body>
+//</html>
+alert("welcome to my bubble tea website!"):
